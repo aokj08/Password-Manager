da6789414a965dc1e5721dc960f1bcc0cef27611
aokj08 <104790843+aokj08@users.noreply.github.com>
2023-11-04 12:07:04 -0700

    removed class

--- Main.py
+++ Main.py
@@ -1,29 +1,28 @@
-class Main: 
-    #Open the connection to the flat file
-    if __name__ == "__main__":
-        returning = input("Welcome to the password manager, are you a returning user? (Y/N): ")
-        while returning != "y" and returning != "Y" and returning != "N" and returning != "n":
-            returning = input("Please enter Y/N only: ")
-        password = input("Please enter your password: ")
-        if returning == "Y" or returning == "y":
-            file = open("password.txt", "r")
-            #Check and verify from the flat file
-            found = False
-            for x in file:
-                if x == password+"\n":
-                    #Grant access
-                    print("Password found!")
-                    found = True
-                    break
-            if not found:
-                print("Password not found.")
-            file.close()
+#Open the connection to the flat file
+if __name__ == "__main__":
+    returning = input("Welcome to the password manager, are you a returning user? (Y/N): ")
+    while returning != "y" and returning != "Y" and returning != "N" and returning != "n":
+        returning = input("Please enter Y/N only: ")
+    password = input("Please enter your password: ")
+    if returning == "Y" or returning == "y":
+        file = open("password.txt", "r")
+        #Check and verify from the flat file
+        found = False
+        for x in file:
+            if x == password+"\n":
+                #Grant access
+                print("Password found!")
+                found = True
+                break
+        if not found:
+            print("Password not found.")
+        file.close()
 
-        else:
-            #Stores it in the flat file. Assuming no collision
-            file = open("password.txt", "a")
-            file.write(password+"\n")
-            file.close()
-            print("Successfully stored your password!")
+    else:
+        #Stores it in the flat file. Assuming no collision
+        file = open("password.txt", "a")
+        file.write(password+"\n")
+        file.close()
+        print("Successfully stored your password!")
 
 
